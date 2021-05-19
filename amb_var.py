e8d0d84cc019f8859919b1ed8d0cefe6bbbac42e
Ch3did <phchedidk@gmail.com>
2021-05-19 12:40:00 -0300

    [UPDATE] - Removing the KEY

--- amb_var.py
+++ amb_var.py
@@ -1,7 +1,11 @@
-KEY = '6NLEKEA6FR9NAY0W'
+#Campo Chave da API
+KEY = ''
+
+#Nome da Database
 DATABASE = "sqlite.db"
+
+#Symbolos para extração na API
 TABLES = (
     'B3SA3.SAO',
-
     'PETR4.SAO'
 )
